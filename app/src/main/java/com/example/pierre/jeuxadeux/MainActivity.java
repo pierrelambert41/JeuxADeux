06bf1a2068ab9b5e14d40a19b0e14e003ca4d84b
plambert <pierre.lambert@mind7.com>
2018-08-20 19:15:28 +0200

    refactor

--- app/src/main/java/com/example/pierre/jeuxadeux/MainActivity.java
+++ app/src/main/java/com/example/pierre/jeuxadeux/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     int scoreJoueur1 = 0;
     int scoreJoueur2 = 0;
@@ -22,54 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //pour Joueurs
         Button buttonJ1 = (Button)findViewById(R.id.button_joueur1);
         Button buttonJ2 = (Button)findViewById(R.id.button_joueur2);
+
+        //Reset
         Button reset = (Button)findViewById(R.id.reset);
 
+        //Jeu
         textJoueur1 = (TextView)findViewById(R.id.scorejoueur1);
         textJoueur2 = (TextView)findViewById(R.id.scorejoueur2);
 
 
-        textJoueur1.setText("Le score est : 0");
-        textJoueur2.setText("Le score est : 0");
+        textJoueur1.setText("Le score est : " + scoreJoueur1);
+        textJoueur2.setText("Le score est : " + scoreJoueur2);
+
+        buttonJ1.setOnClickListener(this);
+        buttonJ2.setOnClickListener(this);
+        reset.setOnClickListener(this);
 
         reset();
-
-
-        buttonJ1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (jeuEnCours) {
-                    scoreJoueur1++;
-                    textJoueur1.setText("Le score est : " + scoreJoueur1);
-                    if (scoreJoueur1 == 10) {
-                        Toast.makeText(MainActivity.this, "Joueur 1 à gagné", Toast.LENGTH_SHORT).show();
-                        jeuEnCours = false;
-                    }
-                }
-            }
-        });
-
-        buttonJ2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (jeuEnCours) {
-                    scoreJoueur2++;
-                    textJoueur2.setText("Le score est : " + scoreJoueur2);
-                    if (scoreJoueur2 == 10) {
-                        Toast.makeText(MainActivity.this, "Joueur 2 à gagné", Toast.LENGTH_SHORT).show();
-                        jeuEnCours = false;
-                    }
-                }
-            }
-        });
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                reset();
-            }
-        });
 
     }
 
@@ -80,6 +52,38 @@ public class MainActivity extends AppCompatActivity {
         textJoueur1.setText("Le score est : " + scoreJoueur1);
         textJoueur2.setText("Le score est : " + scoreJoueur2);
         Toast.makeText(MainActivity.this, "Commencez !", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch(id){
+            case R.id.button_joueur1:
+                //j1 clique
+                if (jeuEnCours) {
+                    scoreJoueur1++;
+                    textJoueur1.setText("Le score est : " + scoreJoueur1);
+                    if (scoreJoueur1 == 10) {
+                        Toast.makeText(MainActivity.this, "Joueur 1 à gagné", Toast.LENGTH_SHORT).show();
+                        jeuEnCours = false;
+                    }
+                }
+                break;
+            case R.id.button_joueur2:
+                //j2 clique
+                if (jeuEnCours) {
+                    scoreJoueur2++;
+                    textJoueur2.setText("Le score est : " + scoreJoueur2);
+                    if (scoreJoueur2 == 10) {
+                        Toast.makeText(MainActivity.this, "Joueur 2 à gagné", Toast.LENGTH_SHORT).show();
+                        jeuEnCours = false;
+                    }
+                }
+                break;
+            case R.id.reset:
+                reset();
+                break;
+        }
     }
 
 }
